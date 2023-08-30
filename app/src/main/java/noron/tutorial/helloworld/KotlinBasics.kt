f1e415b911ec51e16779766ba994973fe2fa0bca
mohi <mohisadeghi302@gmail.com>
2023-08-30 17:51:17 +0330

    Add datatypes

--- app/src/main/java/noron/tutorial/helloworld/KotlinBasics.kt
+++ app/src/main/java/noron/tutorial/helloworld/KotlinBasics.kt
@@ -2,6 +2,15 @@ package noron.tutorial.helloworld
 
 fun main() {
     // var=>mutable, val=>immutable
+    // TODO: Add new function
+    /* Data Types */
+    //Long / Byte / Short / Int / Float / Double
+    val myFloat: Float = 13.37F // more
+    var isSunny: Boolean  = true
+    isSunny = false
+    val letterChar = 'A'
+    val myStr = "Hello World"
+    var firstChar = myStr[0] // last one: myStr.length - 1
     var myName = "Mohi"
-    print("Hello "+ myName)
+    print("Hello "+ myName +"\nLast char: "+ firstChar)
 }
