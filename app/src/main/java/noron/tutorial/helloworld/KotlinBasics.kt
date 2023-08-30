7770387e6f14395cb4776de3f09909ab636f5ecc
mohi <mohisadeghi302@gmail.com>
2023-08-30 18:31:05 +0330

    Add operators

--- app/src/main/java/noron/tutorial/helloworld/KotlinBasics.kt
+++ app/src/main/java/noron/tutorial/helloworld/KotlinBasics.kt
@@ -3,11 +3,26 @@ package noron.tutorial.helloworld
 fun main() {
     // var=>mutable, val=>immutable
     // TODO: Add new function
+
     /* Data Types */
-    //Long / Byte / Short / Int / Float / Double
-    val myFloat: Float = 13.37F // more
-    var isSunny: Boolean  = true
-    isSunny = false
+    // /=, +=, *=
+    // Long / Byte / Short / Int / Float / Double
+    // val myFloat: Float = 13.37F // more
+    // var isSunny: Boolean  = true
+    // isSunny = false
+
+    /* Operator */
+//    val a = 5.0
+//    val b = 3
+//    var resultDouble: Double
+//    resultDouble = a / b
+
+    /* comparison operators */
+//    val isEqual = 5==3
+//    println("isEqual is ${"isEqual"}")
+
+
+    /* String */
     val letterChar = 'A'
     val myStr = "Hello World"
     var firstChar = myStr[0] // last one: myStr.length - 1
