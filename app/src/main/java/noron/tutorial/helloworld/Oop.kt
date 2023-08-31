ec0c694d930480fac3549eb0819d29519c9370e2
mohi <mohisadeghi302@gmail.com>
2023-09-01 01:15:53 +0330

    Getter and Setter

--- app/src/main/java/noron/tutorial/helloworld/Oop.kt
+++ app/src/main/java/noron/tutorial/helloworld/Oop.kt
@@ -6,14 +6,28 @@ fun main() {
 //    mohi.age = 98
     mohi.hobby = "to skateboard"
     john.hobby = "play video games"
-    print( mohi.age)
+//    print( mohi.age)
 }
 
 class Person (firstName: String = "John", lastName: String = "Doe", age : Int = 0 ){
+
+    /* Initializer */
     var age : Int? = null
     var hobby : String = "watch Netflix "
     var firstName: String? = null
     var lastName: String? = null
+
+    /* Getter and Setter */
+    val myBrand: String = "BWM"
+        get() {
+            return field.toLowerCase()
+        }
+
+    var maxSpeed: Int = 200
+//        private set  // only can change in class
+        set(value){
+            field = if(value > 0) value else throw IllegalAccessException("Max speed cannot be less than 0")
+        }
 
 //    constructor(firstName: String, lastName: String, age: Int)
 //            : this(firstName, lastName){
@@ -25,13 +39,14 @@ class Person (firstName: String = "John", lastName: String = "Doe", age : Int = 
         this.lastName = lastName
         this.age = age
     }
+
 //    fun stateAge() {
 //        println("age:$age")
 //    }
 
-    fun stateHobby(){
+//    fun stateHobby(){
 //        println("My hobby is $hobby")
-    }
+//    }
 
 
 }
