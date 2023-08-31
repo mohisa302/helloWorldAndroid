061e45956945fb808e52422e0da70733c3c0586c
mohi <mohisadeghi302@gmail.com>
2023-09-01 02:01:23 +0330

    Inheritence

--- app/src/main/java/noron/tutorial/helloworld/Oop.kt
+++ app/src/main/java/noron/tutorial/helloworld/Oop.kt
@@ -1,11 +1,26 @@
 package noron.tutorial.helloworld
 
+import java.time.temporal.TemporalAmount
+
+data class User(val id: Long, var name: String)
 fun main() {
-    var mohi = Person("Mohi","Sadeghi", 21 )
-    var john = Person()
+
+    /* User */
+//    val user1 = User(1, "Denis")
+//    val user2 = User(2 , "K0u9")
+    // user.copy()
+//    println(user1.equals(user2))
+
+    /* Property */
+//    println(user1.component1()) //id
+//    val (id, name) = user2
+
+    /* Person */
+//    var mohi = Person("Mohi","Sadeghi", 21 )
+//    var john = Person()
 //    mohi.age = 98
-    mohi.hobby = "to skateboard"
-    john.hobby = "play video games"
+//    mohi.hobby = "to skateboard"
+//    john.hobby = "play video games"
 //    print( mohi.age)
 }
 
@@ -49,4 +64,23 @@ class Person (firstName: String = "John", lastName: String = "Doe", age : Int = 
 //    }
 
 
+}
+
+//sub
+open class Car(val name: String, val brand: String ){
+   open var range: Double = 0.0
+
+   fun extendRange(amount: Double){
+       if(amount > 0)
+           range += amount
+   }
+
+  open fun drive(distance: Double){
+      println("Drove for $distance KM")
+  }
+}
+
+//here we have access to all the methods and variables in car class
+class ElectricCar(name: String, brand: String, batteryLife: Double ): Car(name, brand){
+    override var range = batteryLife * 7  //need open in original class
 }
