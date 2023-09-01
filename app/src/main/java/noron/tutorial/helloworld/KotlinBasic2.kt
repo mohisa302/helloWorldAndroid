9a9d6aa2613b0056c5d3cc33557ad1aecd6a2a8b
mohi <mohisadeghi302@gmail.com>
2023-09-01 12:48:36 +0330

    Lambda expression

--- app/src/main/java/noron/tutorial/helloworld/KotlinBasic2.kt
+++ app/src/main/java/noron/tutorial/helloworld/KotlinBasic2.kt
@@ -1,7 +1,13 @@
 package noron.tutorial.helloworld
 
 fun main() {
+    /* Lambda expression */
+    val sum  = {a:Int, b:Int -> println(a+b)}
+
+
+
     /* Functions of Arraylist */
+
     // open fun add -> add specific element
     // open fun clear() -> remove all elements from the collection
     // open fun get -> return the element at specified index in the list.
@@ -9,18 +15,20 @@ fun main() {
     // ArrayList(ele: Collection<E>)
     // open fun remove(ele)
 
-    val arrayList = ArrayList<String>() //empty
-    arrayList.add("One")
-    arrayList.add("Two")
-    for(i in arrayList) {
-        println(i)
-    }
+//    val arrayList = ArrayList<String>() //empty
+//    arrayList.add("One")
+//    arrayList.add("Two")
+//    for(i in arrayList) {
+//        println(i)
+//    }
 
     /* Iterator */
-    val itr = arrayList.iterator()
-    while (itr.hasNext()) {
-        println(itr.next())
-    }
+//    val itr = arrayList.iterator()
+//    while (itr.hasNext()) {
+//        println(itr.next())
+//    }
+//
+//    println(arrayList.get(1))
 
 }
 
